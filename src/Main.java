76d1a9dd8787916269903396ddfdda7dcac98fb1
Berk Çelik <berchoeses@gmail.com>
2020-02-10 00:13:25 +0300

    numeral sum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+
 public class Main {
 
 
 
     public static void main(String[] args) {
 
-        //TODO
-        //Yazdığın fonksiyonu burada çağır.
+        Methods.numeralSum();
 
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Methods {
 
     public static void numeralSum() {
-        //TODO
-        //Kullanıcıdan integer alıp, rakamları toplamını bastıran bir method yaz.
+        Scanner s = new Scanner(System.in);
+        System.out.println("sayi: ");
+        int a = s.nextInt();
+        int toplam;
+        for (toplam = 0; a > 0; a /= 10) {
+            toplam += a % 10;
+        }
+        System.out.println("rakamlar toplami: " + toplam);
     }
 
 
