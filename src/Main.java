293f687fda007ab5c013380deb27408d57fe8f3b
Berk Çelik <berchoeses@gmail.com>
2020-02-09 23:51:07 +0300

    rakamlari topla

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        //TODO
+        //Yazdığın fonksiyonu burada çağır.
+
+    }
+}
---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Methods {
+
+    public static void numeralSum() {
+        //TODO
+        //Kullanıcıdan integer alıp, rakamları toplamını bastıran bir method yaz.
+    }
+
+
+}
